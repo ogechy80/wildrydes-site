fd7aef80704ffc6d388137e795a5240bc2e4381e
ogechy80 <125331603+ogechy80@users.noreply.github.com>
2025-02-26 19:26:27 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_iloXWtudY', // e.g. us-east-1_uXboG5pAb
+        userPoolClientId: 'r86vgfu8dt2nie12f2kbs5s6h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-1.amazonaws.com/prod',
     }
 };
